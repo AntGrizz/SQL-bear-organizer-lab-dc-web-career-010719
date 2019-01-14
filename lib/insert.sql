5f6af48c31afcd25343542d26c1bc5d652d01186
Anthony Gregg <agregg22@gmail.com>
2019-01-13 21:56:03 -0500

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 21, 'M', 'Dark-Brown', 'Laid-back', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 18, 'M', 'Dark-Brown', 'Goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 19, 'F', 'Light-Brown', 'Sexy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 25, 'M', 'Dark-Brown', 'Uptight', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'Brown', 'Shy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 30, 'M', 'Green', 'Mischievous', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 21, 'F', 'Light-Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 27, 'M', 'Dark-Brown', 'Mean', 0);
